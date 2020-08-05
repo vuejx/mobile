71290aecda248e1953e906f05057f6c2df3a0837
binhthvuejx <binhth.vuejx@gmail.com>
2020-08-05 15:23:35 +0700

    demo

---
+++ demo/app/components/App.vue
@@ -0,0 +1,77 @@
+<template>
+    <Page>
+        <ActionBar>
+            <GridLayout width="100%" columns="auto, *">
+                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
+                <Label class="title" text="Welcome to NativeScript-Vue!"  col="1"/>
+            </GridLayout>
+        </ActionBar>
+
+        <RadSideDrawer ref="drawer">
+            <StackLayout ~drawerContent backgroundColor="#ffffff">
+                <Label class="drawer-header" text="Drawer"/>
+
+                <Label class="drawer-item" text="Item 1"/>
+                <Label class="drawer-item" text="Item 2"/>
+                <Label class="drawer-item" text="Item 3"/>
+            </StackLayout>
+
+            <GridLayout ~mainContent columns="*, 2*" rows="*">
+                <StackLayout col="0">
+                    <Button text="inline style" class="text-white bg-black"/>
+                    <Label text="first222" height="70" backgroundColor="#123123" />
+                </StackLayout>
+                <StackLayout col="1">
+                    <Label text="first 2222" height="70" backgroundColor="#43b883" />
+                    <Label text="first222 333" height="70" backgroundColor="#123123" />
+                </StackLayout>
+            </GridLayout>
+        </RadSideDrawer>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style>
+    @import url('../vuejx.css');
+</style>
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .title {
+        text-align: left;
+        padding-left: 16;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+
+    .drawer-header {
+        padding: 50 16 16 16;
+        margin-bottom: 16;
+        background-color: #53ba82;
+        color: #ffffff;
+        font-size: 24;
+    }
+
+    .drawer-item {
+        padding: 8 16;
+        color: #333333;
+        font-size: 16;
+    }
+</style>
