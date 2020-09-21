fc0ad952c34b8352b8c7c8374b3d661cfa3f2eb6
binhthvuejx <binhth.vuejx@gmail.com>
2020-09-21 12:50:31 +0700

    commit

--- opencps_pilot/app/components/button/Index.vue
+++ opencps_pilot/app/components/button/Index.vue
@@ -1,83 +1,78 @@
 <template>
-  <GridLayout :columns="'auto, *, auto'" :rows="'*'" @tap="click" iosOverflowSafeArea="false" :class="{'tapPendding': label && tapPendding, 'tapPenddingText': label === '' && tapPendding}">
-      <GridLayout :col="'0'" iosOverflowSafeArea="false" v-if="appendIcon">
+  <GridLayout :columns="'auto, *, auto'" rows="*,auto,*" @tap="click" iosOverflowSafeArea="false" :class="{'tapPendding': label && tapPendding, 'tapPenddingText': label === '' && tapPendding}">
+      <GridLayout col="0" row="1" iosOverflowSafeArea="false" v-if="appendIcon">
           <vn-image-view :height="iconSize" :width="iconSize" :src_icon="appendIcon" :tintColor="tintColor" :class="iconClass" />
       </GridLayout>
-      <GridLayout :col="'1'">
-          <Label textWrap="true" :textAlignment="textAlignment" :text="label" :class="labelCalss" />
+      <GridLayout col="1" row="1" cols="*" rows="*,auto,*">
+          <Label col="0" row="1" textWrap="true" :textAlignment="textAlignment" :text="label" :class="labelCalss" />
       </GridLayout>
-      <GridLayout :col="'2'" iosOverflowSafeArea="false" v-if="prependIcon">
+      <GridLayout col="2" row="1" iosOverflowSafeArea="false" v-if="prependIcon">
           <vn-image-view :height="iconSize" :width="iconSize" :src_icon="prependIcon" :tintColor="tintColor" :class="iconClass" />
       </GridLayout>
   </GridLayout>
 </template>
 
-<script >
-export default {
-  props:{
-  	label: {
-      type: String
-    },
-    tintColor: {
-      type: String,
-      default: ''
-    },
-  	iconClass: {
-      type: String,
-      default: ''
-    },
-  	textAlignment: {
-      type: String,
-      default: 'left'
-    },
-  	iconFill: {
-      type: String,
-      default: 'black'
-    },
-  	iconSize: {
-      type: String,
-      default: '20'
-    },
-  	appendIcon: {
-      type: String,
-      default: ''
-    },
-  	prependIcon: {
-      type: String,
-      default: ''
-    },
-    icon: {
-      type: Boolean,
-      default: false
-    },
-  	labelCalss: {
-      type: String,
-      default: ''
-    },
-  },
-  data() {
-    return {
-      tapPendding: false
+<script>
+    export default {
+        props: {
+            label: {
+                type: String
+            },
+            tintColor: {
+                type: String,
+                default: ""
+            },
+            iconClass: {
+                type: String,
+                default: ""
+            },
+            textAlignment: {
+                type: String,
+                default: "left"
+            },
+            iconFill: {
+                type: String,
+                default: "black"
+            },
+            iconSize: {
+                type: String,
+                default: "20"
+            },
+            appendIcon: {
+                type: String,
+                default: ""
+            },
+            prependIcon: {
+                type: String,
+                default: ""
+            },
+            icon: {
+                type: Boolean,
+                default: false
+            },
+            labelCalss: {
+                type: String,
+                default: ""
+            }
+        },
+        data() {
+            return {
+                tapPendding: false
+            };
+        },
+        methods: {
+            click() {
+                this.$emit("tap");
+            }
+        }
     };
-  },
-  methods:{
-    click(){
-      if (!this.tapPendding) {
-        this.$emit('tap')
-      }
-      this.tapPendding = true;
-      setTimeout(() => {
-        this.tapPendding = false;
-      }, 200);
-    },    
-  }
-};
 </script>
 <style>
-  .tapPendding {
-    background-color: darkgray;
-  }
-  .tapPenddingText {
-    color: darkgray;
-  }
+    .tapPendding {
+        background-color: darkgray;
+    }
+
+    .tapPenddingText {
+        color: darkgray;
+    }
 </style>
